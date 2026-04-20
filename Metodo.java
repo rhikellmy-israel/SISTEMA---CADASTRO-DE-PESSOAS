f79b368c73e7e070c2ed3bc29ab19ed42d6e4564
Rhikellmy <125161157+rhikellmy-israel@users.noreply.github.com>
2026-04-19 22:23:00 -0300

    Add files via upload

--- Metodo.java
+++ Metodo.java
@@ -1,3 +1,59 @@
+import java.util.Scanner;
+import java.util.List;
+
 public class Metodo {
+    //Método para cadastrar uma pessoa
+    public static void push(List<Pessoa> Lista) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o nome da pessoa: ");
+        String nome = sc.nextLine();
+        sc.nextLine();
+
+        System.out.println("Informe a idade da pessoa: ");
+        int idade = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Informe o CPF da pessoa: ");
+        Double cpf = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("Digite a profissáo da pessoa: ");
+        String profissao = sc.nextLine();
+        sc.nextLine();
+
+        Pessoa pessoa = new Pessoa(nome, idade, cpf, profissao);
+        lista.add(pessoa);
+
+        System.out.println("Pessoa cadastrada com sucesso!");
+    }
     
+
+    // Método para mostrar toda a lista de pessoas cadastradas
+    public static void listaPessoa() {
+        if (lista.isEmpty()) {
+            System.out.println("A lista esta vazia");
+        } else {
+            for (Pessoa pessoaAtual : lista) {
+                System.out.println("Nome: " + nome);
+                System.out.println("Idade: " + idade);
+                System.out.println("CPF: " + cpf);
+                System.out.println("Profissão: " + profissao);
+                System.out.println("----------");
+            }
+        }
+    }
+
+    // Método para deletar um cadastro de pessoa
+
+    public static void deletar(List<Pessoa> lista ; String nomeParaDeletar){
+        for(int i = 0; i < lista.size; i++){
+            Pessoa p = lista.get(i);
+            if(p.getNome().equalsIgnoreCase(nomeParaDeletar)){
+                lista.remove(i);
+                System.out.println("Pessoa Deletada da lista com sucesso!");
+            }
+        }
+        System.out.println("Não foi possivel encontrar a pessoa mencionada");
+    }
 }
